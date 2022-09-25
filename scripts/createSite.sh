b5b9d08f4da1152a80e95fa4365b1fab0f213130
Adrian Filip <realadrianfilip@gmail.com>
2022-09-25 19:21:58 +0300

    Update createSite.sh remove series logic (#41)

--- scripts/createSite.sh
+++ scripts/createSite.sh
@@ -17,18 +17,6 @@ cd zio-connect
 sbt docs/mdoc
 sbt docs/unidoc
 
-mkdir -p "$original_dir"/website/versioned_docs/version-0.x
-mv zio-connect-docs/target/mdoc/* "$original_dir"/website/versioned_docs/version-0.x
-
-mkdir -p "$original_dir"/website/static/api/0.x
-mv website/static/api "$original_dir"/website/static/api-0.x
-
-# Now we need to checkout the branch that originally has triggered the site build
-cd "$original_dir"
-git fetch --tags
-ZIO_CONNECT_LATEST_2=`git describe --tags --abbrev=0 ` sbt docs/unidoc
-ZIO_CONNECT_LATEST_2=`git describe --tags --abbrev=0 ` sbt docs/mdoc
-
 cd website 
 rm -Rf node_modules
 yarn install 
