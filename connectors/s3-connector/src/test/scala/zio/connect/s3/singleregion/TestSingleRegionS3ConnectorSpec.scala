b0a144942488f58a65ab85905b80f6ce05f69688
Adrian Filip <realadrianfilip@gmail.com>
2022-11-13 14:38:24 +0200

    fmt & fix

--- connectors/s3-connector/src/test/scala/zio/connect/s3/singleregion/TestSingleRegionS3ConnectorSpec.scala
+++ connectors/s3-connector/src/test/scala/zio/connect/s3/singleregion/TestSingleRegionS3ConnectorSpec.scala
@@ -2,7 +2,6 @@ package zio.connect.s3.singleregion
 
 import zio.connect.s3.s3ConnectorTestLayer
 
-
 object TestSingleRegionS3ConnectorSpec extends SingleRegionS3ConnectorSpec {
 
   override def spec =
