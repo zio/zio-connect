9f95f2b8fd3b67445c219994c1f106bec98ee823
Adrian Filip <realadrianfilip@gmail.com>
2022-11-17 19:43:08 +0200

    Update CouchbaseConnector.scala

--- connectors/couchbase-connector/src/main/scala/zio/connect/couchbase/CouchbaseConnector.scala
+++ connectors/couchbase-connector/src/main/scala/zio/connect/couchbase/CouchbaseConnector.scala
@@ -35,7 +35,7 @@ object CouchbaseConnector {
   object ScopeName extends Subtype[String]
   type ScopeName = ScopeName.Type
 
-  case class CouchbaseException(reason: Throwable)
+  final case class CouchbaseException(reason: Throwable)
 
   final case class ContentQueryObject(
     bucketName: BucketName,
