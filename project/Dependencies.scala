23a7330a9b4a0ff451221603be39db631d11aeb7
Adrian Filip <realadrianfilip@gmail.com>
2022-11-18 21:31:10 +0200

    Upgrade to zio 2.0.4 (#126)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   val `scala-compact-collection` = "org.scala-lang.modules" %% "scala-collection-compat" % "2.8.1"
 
-  val zioVersion     = "2.0.3"
+  val zioVersion     = "2.0.4"
   val zio            = "dev.zio" %% "zio"          % zioVersion
   val `zio-streams`  = "dev.zio" %% "zio-streams"  % zioVersion
   val `zio-test`     = "dev.zio" %% "zio-test"     % zioVersion % "test"
