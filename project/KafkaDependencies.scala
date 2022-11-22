73b92e393bc9b3a496af13b7b30bff2a03b3e418
Adrian Filip <realadrianfilip@gmail.com>
2022-11-23 00:26:41 +0200

    Format

--- project/KafkaDependencies.scala
+++ project/KafkaDependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object KafkaDependencies {
 
-  lazy val zioKafkaVersion   = "2.0.1"
-  lazy val `zio-kafka` = "dev.zio" %% "zio-kafka" % zioKafkaVersion
-  lazy val `zio-kafka-test-utils`    = "dev.zio" %% "zio-kafka-test-utils"    % zioKafkaVersion % "test"
+  lazy val zioKafkaVersion        = "2.0.1"
+  lazy val `zio-kafka`            = "dev.zio" %% "zio-kafka"            % zioKafkaVersion
+  lazy val `zio-kafka-test-utils` = "dev.zio" %% "zio-kafka-test-utils" % zioKafkaVersion % "test"
 
 }
