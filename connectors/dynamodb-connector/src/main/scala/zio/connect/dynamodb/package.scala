864fa27be9a189b26ae122a9f39b5ac34c1c7328
Ahmad Ragab <Averroes2006@gmail.com>
2022-11-27 05:15:13 -0500

    Adding methods to package

--- connectors/dynamodb-connector/src/main/scala/zio/connect/dynamodb/package.scala
+++ connectors/dynamodb-connector/src/main/scala/zio/connect/dynamodb/package.scala
@@ -2,22 +2,43 @@ package zio.connect
 
 import zio.aws.core.AwsError
 import zio.aws.dynamodb.DynamoDb
-import zio.aws.dynamodb.model.primitives.TableName
+import zio.aws.dynamodb.model.primitives.{AttributeName, TableName}
 import zio.aws.dynamodb.model._
-import zio.stream.{ZSink, ZStream}
+import zio.stream.{ZPipeline, ZSink, ZStream}
 import zio.{RLayer, Trace, ULayer}
 
 package object dynamodb {
+
+  def batchGetItem(request: => BatchGetItemRequest)(implicit
+    trace: Trace
+  ): ZStream[DynamoDBConnector, AwsError, BatchGetItemResponse] =
+    ZStream.serviceWithStream[DynamoDBConnector](_.batchGetItem(request))
+
+  def batchWriteItem(implicit
+    trace: Trace
+  ): ZPipeline[DynamoDBConnector, AwsError, BatchWriteItemRequest, BatchWriteItemResponse] =
+    ZPipeline.serviceWithPipeline[DynamoDBConnector](_.batchWriteItem)
 
   def createTable(implicit
     trace: Trace
   ): ZSink[DynamoDBConnector, AwsError, CreateTableRequest, CreateTableRequest, Unit] =
     ZSink.serviceWithSink[DynamoDBConnector](_.createTable)
 
+  def deleteItem(implicit trace: Trace): ZSink[DynamoDBConnector, AwsError, DeleteItemRequest, Nothing, Unit] =
+    ZSink.serviceWithSink[DynamoDBConnector](_.deleteItem)
+
+  def deleteTable(implicit trace: Trace): ZSink[DynamoDBConnector, AwsError, DeleteTableRequest, Nothing, Unit] =
+    ZSink.serviceWithSink[DynamoDBConnector](_.deleteTable)
+
   def describeTable(request: => TableName)(implicit
     trace: Trace
   ): ZStream[DynamoDBConnector, AwsError, TableDescription] =
     ZStream.serviceWithStream[DynamoDBConnector](_.describeTable(request))
+
+  def listTables(request: => ListTablesRequest)(implicit
+    trace: Trace
+  ): ZStream[DynamoDBConnector, AwsError, TableName] =
+    ZStream.serviceWithStream[DynamoDBConnector](_.listTables(request))
 
   def getItem(request: => GetItemRequest)(implicit
     trace: Trace
@@ -27,8 +48,20 @@ package object dynamodb {
   def putItem(implicit trace: Trace): ZSink[DynamoDBConnector, AwsError, PutItemRequest, PutItemRequest, Unit] =
     ZSink.serviceWithSink[DynamoDBConnector](_.putItem)
 
+  def query(request: => QueryRequest): ZStream[DynamoDBConnector, AwsError, Map[AttributeName, AttributeValue]] =
+    ZStream.serviceWithStream[DynamoDBConnector](_.query(request))
+
+  def scan(request: => ScanRequest): ZStream[DynamoDBConnector, AwsError, Map[AttributeName, AttributeValue]] =
+    ZStream.serviceWithStream[DynamoDBConnector](_.scan(request))
+
   def tableExists(implicit trace: Trace): ZSink[DynamoDBConnector, AwsError, TableName, TableName, Boolean] =
     ZSink.serviceWithSink[DynamoDBConnector](_.tableExists)
+
+  def updateItem(implicit trace: Trace): ZSink[DynamoDBConnector, AwsError, UpdateItemRequest, Nothing, Unit] =
+    ZSink.serviceWithSink[DynamoDBConnector](_.updateItem)
+
+  def updateTable(implicit trace: Trace): ZSink[DynamoDBConnector, AwsError, UpdateTableRequest, Nothing, Unit] =
+    ZSink.serviceWithSink[DynamoDBConnector](_.updateTable)
 
   val dynamoDBConnectorLiveLayer: RLayer[DynamoDb, LiveDynamoDBConnector] = LiveDynamoDBConnector.layer
   val dynamoDBConnectorTestLayer: ULayer[TestDynamoDBConnector]           = TestDynamoDBConnector.layer
